b31a0bb57255b71af3ea4067ba971a52226c56e7
shwetajoshi-15 <54519592+shwetajoshi-15@users.noreply.github.com>
2020-08-12 16:17:49 +1000

    Update parse_maf_transcripts_04.pl

--- scripts/parse_maf_transcripts_04.pl
+++ scripts/parse_maf_transcripts_04.pl
@@ -56,7 +56,7 @@ if(scalar keys %selected_reads > 0){
 	open(OUT5, ">$outpath/selected_reads_potential_exons.fa");
 	GET_SEQ($fasta_2D);
 	close OUT5;
-	system("lastal $genomeDB $outpath/selected_reads_potential_exons.fa > $outpath/selected_reads_potential_exons.maf \n")==0||die"FAIL LAST $genomeDB \n";
+	system("lastal -P $threads $genomeDB $outpath/selected_reads_potential_exons.fa > $outpath/selected_reads_potential_exons.maf \n")==0||die"FAIL LAST $genomeDB \n";
 }
 ########################################################
 #parse the annotation file to generate a bed file of the exon positions and an hash associating positions within transcripts to genomic positions 
@@ -70,14 +70,16 @@ sub FLATTEN_GTF{
 		if($_=~/\texon\t/){
 			chomp;
 			my $line=$_;
-			$line=~/gene_id \"([0-9a-zA-Z_.]+)\";/;
+			$line=~/gene_id \"([0-9a-zA-Z_]+)\";/;
 			my $geneid=$1;
-			$line=~/transcript_id \"([0-9a-zA-Z_.]+)\";/;
+			$line=~/transcript_id \"([0-9a-zA-Z_]+)\";/;
 			my $transid=$1;
 			$_=~s/\"//g;
 			$_=~s/;//g;
 			$_=~s/[\s]+/\t/g;
 			my @split=split /\t/, $_;
+			$transid=$split[11];
+			$geneid=$split[9];
 			$transcripts{$transid}=$geneid;
 			push @{$temp_trans{$transid}}, [$split[0], $split[3], $split[4], $split[6], $transid];
 			push @{$temp_chr{$split[0]}}, [$split[0], $split[3], $split[4], $split[6], $geneid, $transid];
